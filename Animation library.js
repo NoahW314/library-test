2e7443109dede2612c1a6c4adc12a2f046ab9c45
NoahW314 <noahwalker3.14@gmail.com>
2017-09-04 17:31:39 -0400

    o

--- Animation library.js
+++ Animation library.js
@@ -1,6 +1,6 @@
+//multiple finishes at once
 //add "wait" in combination with other finish(es) to finish
 //add error messages?
-//add stop to finish
 //add number of times to loop through array of finishes
 //add conditions instead of time limits?
 //choice to base min and max on x or y
@@ -23,6 +23,7 @@ var Counter = function (x, min, max, finish, f, fr, maxTimes, finish2, finish3) 
 	this.fCount = 0;
 	this.finish2 = finish2;
 	this.finish3 = finish3;
+	this.finishCount = 0;
 	if(Array.isArray(this.min)){
 		this.minArray = min;
 		this.maxArray = max;
@@ -33,6 +34,57 @@ var Counter = function (x, min, max, finish, f, fr, maxTimes, finish2, finish3) 
 		this.fr = undefined;
 		this.maxTimes = undefined;
 	}
+	if(!Array.isArray(this.finish)){
+		this.finish = [this.finish];
+	}
+};
+Counter.prototype.functionFinish = function () {
+	if(typeof this.finish[this.finishCount] === "function"){
+		this.run = false;
+		this.finish[this.finishCount]();
+	}
+};
+Counter.prototype.restartFinish = function () {
+	this.x = 0;
+};
+Counter.prototype.returnFinish = function () {
+	this.rtrn = !this.rtrn;
+	this.finish3();
+	this.times++;
+	if(this.times >= this.maxTimes){
+		this.run = false;
+		this.finish2();
+	}
+};
+Counter.prototype.resetFinish = function () {
+	this.y = this.f(0);
+	this.run = false;
+};
+Counter.prototype.arrayCountersFinish = function () {
+	if(this.fCount === this.f.length-1){
+		if(typeof this.finish[this.finishCount] === "function"){
+			this.run = false;
+			this.finish[this.finishCount]();
+		}
+		else if(this.finish[this.finishCount] === "loop"){
+			this.fCount = 0;
+			this.x = this.minArray[0];
+			this.min = this.minArray[0];
+			this.max = this.maxArray[0];
+			this.finish3();
+		}
+		else if(this.finish[this.finishCount] === "stop"){this.run = false;}
+	}
+	else{
+		this.fCount++;
+		this.x = this.minArray[this.fCount];
+		this.min = this.minArray[this.fCount];
+		this.max = this.maxArray[this.fCount];
+		this.finish2();
+	}
+};
+Counter.prototype.stopFinish = function () {
+	this.run = false;
 };
 Counter.prototype.count = function () {
 	if(this.run){
@@ -56,17 +108,12 @@ Counter.prototype.count = function () {
 			}
 		}
 		if(this.y >= this.max || this.y <= this.min){
-			if(typeof this.finish === "function"){
+			if(typeof this.finish[this.finishCount] === "function"){
 				this.run = false;
-				this.finish();
+				this.finish[this.finishCount]();
 			}
-			else if(Array.isArray(this.finish)){
-				for(var i = 0; i < this.finish.length; i++){
-					this.finish[i]();
-				}
-			}
-			else if(this.finish === "restart"){this.x = 0;}
-			else if(this.finish === "return"){
+			else if(this.finish[this.finishCount] === "restart"){this.x = 0;}
+			else if(this.finish[this.finishCount] === "return"){
 				this.rtrn = !this.rtrn;
 				this.finish3();
 				this.times++;
@@ -75,20 +122,21 @@ Counter.prototype.count = function () {
 					this.finish2();
 				}
 			}
-			else if(this.finish === "reset"){this.y = this.f(0); this.run = false;}
+			else if(this.finish[this.finishCount] === "reset"){this.y = this.f(0); this.run = false;}
 			else if(Array.isArray(this.f)){
 				if(this.fCount === this.f.length-1){
-					if(typeof this.finish === "function"){
+					if(typeof this.finish[this.finishCount] === "function"){
 						this.run = false;
-						this.finish();
+						this.finish[this.finishCount]();
 					}
-					else if(this.finish === "loop"){
+					else if(this.finish[this.finishCount] === "loop"){
 						this.fCount = 0;
 						this.x = this.minArray[0];
 						this.min = this.minArray[0];
 						this.max = this.maxArray[0];
 						this.finish3();
 					}
+					else if(this.finish[this.finishCount] === "stop"){this.run = false;}
 				}
 				else{
 					this.fCount++;
@@ -97,6 +145,12 @@ Counter.prototype.count = function () {
 					this.max = this.maxArray[this.fCount];
 					this.finish2();
 				}
+			}
+			else if(this.finish[this.finishCount] === "stop"){this.run = false;}
+			
+			this.finishCount++;
+			if(this.finishCount >= this.finish.length){
+				this.finishCount = 0;
 			}
 		}
 	}

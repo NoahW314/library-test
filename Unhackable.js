bc4e369e14bb487a6d0f6d43e2ef2998043b9d1f
NoahW314 <noahwalker3.14@gmail.com>
2017-10-10 18:58:07 -0400

    d3

--- Unhackable.js
+++ Unhackable.js
@@ -1,28 +1,10 @@
-/*var runObject = {};
-Object.defineProperty(runObject, "run", {
-	value: 
-	function(){
-		var counter = 0;
-	
-		document.addEventListener("click", function(){
-			counter++;
-			var clicks = document.getElementById("clicks");
-			clicks.innerHTML = counter;
-		});
-	},
-	writeable: false,
-	configurable: false,
-});*/
-
 (function(){
 	var counter = 0;
 
 	document.addEventListener("click", function(){
 		counter++;
 		var clicks = document.getElementById("clicks");
-		clicks.innerHTML = "Coins"+counter;
-		
-		throw document.getElementById("wood-mine");
+		clicks.innerHTML = "Coins: "+counter;
 	});
 })();
 

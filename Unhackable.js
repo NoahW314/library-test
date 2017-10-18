b607d1858a8fb135271315b07ad2069cc0efb7fb
NoahW314 <noahwalker3.14@gmail.com>
2017-10-17 22:01:18 -0400

    f

--- Unhackable.js
+++ Unhackable.js
@@ -1,11 +1,25 @@
 (function(){
 	var counter = 0;
+	var mineCounters = [];
+	var touchCounters = [];
 
-	document.addEventListener("click", function(){
+	var clicker = document.getElementById("main-clicker");
+	var items = ["wood", "metal", "gold", "planetary", "galactic", "universal"];
+	var types = ["mine", "touch"];
+	for(var t = 0; t < types.length; t++){
+		for(var i = 0; i < items.length; i++){
+			var item = document.getElementById(types[t]+"-"+items[i]);
+			item.addEventListener("click", function () {
+				throw this;
+			});
+		}
+	}
+	
+	clicker.addEventListener("click", function(){
 		counter++;
 		var clicks = document.getElementById("clicks");
 		clicks.innerHTML = "Coins: "+counter;
 	});
+	
+	
 })();
-
-
